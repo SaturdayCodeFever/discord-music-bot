d5ad45249e917d4283d06d347716be1221d2506f
jeanmassip <oriolu.gin@gmail.com>
2020-05-16 20:58:18 +0200

    [Dev] Ajout de la classe commande, contenant les infos necessaire a l'execution de celle ci

---
+++ src/Commands/Command.js
@@ -0,0 +1,7 @@
+class Command {
+    constructor(command, description, callback) {
+        this.command = command;
+        this.description = description;
+        this.callback = callback;
+    }
+}
