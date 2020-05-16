c3701b93e53ff68078f193de2d1ef28801a07a03
jeanmassip <jean.massip@yahoo.com>
2020-05-16 13:48:03 +0200

    Refactoring part 2

--- src/JarvisBot/JarvisBot.js
+++ src/JarvisBot/JarvisBot.js
@@ -1,31 +1,73 @@
+/**
+ * IMPORTS :
+ *      ytdl-core : handles youtube link streaming
+ */
+const ytdl = require('ytdl-core')
+ /**
+  * INSTANCES :
+  *     HANDLED_COMMANDS - Data object containing all the handled commands
+  */
 const HANDLED_COMMANDS = require('Commands.js')
 
+
+/**
+ *  Class containing all the bot logic
+ */
 class JarvisBot {
     constructor(prefix) {
         this.prefix = prefix;
     } 
 
+    //applies the correct logic to the command, based on HANDLED_COMMANDS
     commandHandler(message) {
         let args = message.content.substring(this.prefix.length).split(" ");
+        let command = args[0]
 
 
-        if(Object.values(HANDLED_COMMANDS).includes(args[0])) {
-            if(Object.values(HANDLED_COMMANDS.MUSIC_COMMANDS).includes(args[0])) {
-                switch (args[1]) {
+        if(Object.values(HANDLED_COMMANDS).includes(command)) {
+            if(Object.values(HANDLED_COMMANDS.MUSIC_COMMANDS).includes(command)) {
+                switch (command) {
                     case HANDLED_COMMANDS.MUSIC_COMMANDS.PLAY:
+                        if(!args[1]) {
+                            message.channel.send("Je ne peut rien faire sans lien, monsieur");
+                            return;
+                        }
+
+                        if(!message.member.voice.channel) {
+                            message.channel.send("Pourriez-vous rejoindre un channel vocal avant, monsieur ?");
+                            return;
+                        }
+                        
+                        if(!message.guild.voiceConnection) {
+                            message.member.voice.channel.join()
+                            .then(connection => {
+                                this.play(connection, args[1])
+                            })
+                        } else {
+                            this.play(message.guild.voiceConnection, args[1])
+                        }
+
                         break;
                     case HANDLED_COMMANDS.MUSIC_COMMANDS.PAUSE:
                         break;
                     case HANDLED_COMMANDS.MUSIC_COMMANDS.STOP:
-                        break;
-                    default :
-                        message.channel.send("Je ne peut rien faire sans lien, monsieur.");
                         break;
                 }
             }
         } else {
             message.channel.send("Desole monsieur, cela ne fait pas parti de mes fonctionnalitees. Entrez !help pour plus d'informations.");
         }
+    }
+
+    //Start streaming a youtube url link
+    play(connection, url) {
+        let dispatcher = connection.play(ytdl(url, {
+            filter: "audioonly"
+        }))
+        
+        dispatcher.on("end", () => {
+           connection.disconnect()
+        })
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,60 +1,21 @@
+/**
+ * IMPORTS :
+ *      Discord - package consuming the discord API
+ *      fs - nodejs standard file system package
+ */
 const Discord = require('discord.js');
+const JarvisBot = require('./JarvisBot/JarvisBot')
 const fs = require('fs');
-const ytdl = require('ytdl-core');
-const bot = new Discord.Client();
+
+/**
+ *  INSTANCES :
+ */
 const config = JSON.parse(fs.readFileSync("./config.json"));
+const client = new Discord.Client();
+const jarvis = new JarvisBot(config.prefix);
 
-bot.on('ready', () => {
-    console.log("Jarvis ONLINE");
+client.on('ready', () => {});
 
-    let queue = [];
-});
+client.on('message', jarvis.commandHandler);
 
-bot.on('message', message => {
-    let args = message.content.substring(config.prefix.length).split(" ");
-
-    switch(args[0]) {
-        case 'play':
-            const play = (connection, url) => {
-                 let dispatcher = connection.play(ytdl(url, {
-                     filter: "audioonly"
-                 }))
-                 
-                 dispatcher.on("end", () => {
-                    connection.disconnect()
-                 })
-            }
-
-            if(!args[1]) {
-                message.channel.send("Je ne peut rien faire sans lien, monsieur");
-                return;
-            }
-
-            if(!message.member.voice.channel) {
-                message.channel.send("Pourriez-vous rejoindre un channel vocal avant, monsieur ?");
-                return;
-            }
-
-            if(!message.guild.voiceConnection) {
-                message.member.voice.channel.join()
-                .then(connection => {
-                    message.channel.send(`Vous venez d'ajouter ${args[1]} à la liste d'attente, monsieur`);
-                    queue.push(args[1])
-                    play(connection, queue[0]);
-                })
-            }  
-            
-           break;
-        case 'queue':
-            message.channel.send(`La liste d'attente actuelle est composée des morceaux suivants: `);
-            queue.forEach(song => {
-                message.channel.send(`${song} - \n `);
-            })
-            break;
-        default :
-            break; 
-    }
-
-})
-
-bot.login(config.bot_token);
+client.login(config.bot_token);
